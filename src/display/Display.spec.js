ff0fc6c5a617dd78ea102e33e18d81221d9ec6f9
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-10 17:22:57 -0400

    added display tests for rendering

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Display from './Display';
+
+afterEach(cleanup);
+
+describe('<Display />', () => {
+    it('renders without crashing', () => {
+        render(<Display />);
+    });
+
+    it('renders all the elements properly', () => {
+        const { getByText } = render(<Display />);
+        getByText('', { selector: 'div.display' });
+        getByText('Unlocked', { selector: 'div.led' });
+        getByText('Open', { selector: 'div.led' });
+    });
+});
