8f12a8ea83ce35ed925f342342238e2354f2f767
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-10 15:36:42 -0400

    added dashboard tests for rendering

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
 // Test away
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+afterEach(cleanup);
+
+describe('<Dashboard />', () => {
+    it('renders without crashing', () => {
+        render(<Dashboard />);
+    });
+
+    it('renders all the elements (display & controls) properly', () => {
+        const { getByText } = render(<Dashboard />);
+        getByText('', { selector: 'div.display' });
+        getByText('', { selector: 'div.controls' });
+    });
+});
