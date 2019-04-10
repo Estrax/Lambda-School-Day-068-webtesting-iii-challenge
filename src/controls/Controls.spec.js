8673748a18b6491452b881b7844ec8a999238796
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-04-10 17:20:49 -0400

    added controls tests for rendering

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
 // Test away!
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+afterEach(cleanup);
+
+describe('<Controls />', () => {
+    it('renders without crashing', () => {
+        render(<Controls />);
+    });
+
+    it('renders all the elements properly', () => {
+        const { getByText } = render(<Controls />);
+        getByText('', { selector: 'div.controls' });
+        getByText('Lock Gate', { selector: 'button.toggle-btn' });
+        getByText('Close Gate', { selector: 'button.toggle-btn' });
+    });
+});
